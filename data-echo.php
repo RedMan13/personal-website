b57f9efd4685848dbd929b462b3c1ea279ca2515
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2024-06-07 20:09:50 -0700

    Update data-echo.php

--- data-echo.php
+++ data-echo.php
@@ -1,4 +1,6 @@
-<?php 
-# ensure that we never echo back anything larger then 50mb
-echo json_encode([round(microtime(true) * 1000), substr($_GET['data'], 0, 500)]);
-?>
+<?= json_encode([
+    # convert the unix (seconds) time stamp to js (miliseconds) timestamp
+    time() * 1000, 
+    # ensure that we never echo back anything larger then 50mb
+    substr($_GET['data'], 0, 500)
+]); ?>
