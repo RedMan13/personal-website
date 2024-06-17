8f3bf3a64b5ee770d3c9430ef0649f323b94f9f6
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2024-06-17 16:16:49 -0700

    im sure this is totally done enough to push to public!

---
+++ discordnt/codeSrc/index.js
@@ -0,0 +1,281 @@
+import ApiInterface from "./interface.js";
+import { Asset } from "./asset-helper.js";
+
+const permBits = [
+    "CREATE_INSTANT_INVITE",
+    "KICK_MEMBERS",
+    "BAN_MEMBERS",
+    "ADMINISTRATOR",
+    "MANAGE_CHANNELS",
+    "MANAGE_GUILD",
+    "ADD_REACTIONS",
+    "VIEW_AUDIT_LOG",
+    "PRIORITY_SPEAKER",
+    "STREAM",
+    "VIEW_CHANNEL",
+    "SEND_MESSAGES",
+    "SEND_TTS_MESSAGES",
+    "MANAGE_MESSAGES",
+    "EMBED_LINKS",
+    "ATTACH_FILES",
+    "READ_MESSAGE_HISTORY",
+    "MENTION_EVERYONE",
+    "USE_EXTERNAL_EMOJIS",
+    "VIEW_GUILD_INSIGHTS",
+    "CONNECT",
+    "SPEAK",
+    "MUTE_MEMBERS",
+    "DEAFEN_MEMBERS",
+    "MOVE_MEMBERS",
+    "USE_VAD",
+    "CHANGE_NICKNAME",
+    "MANAGE_NICKNAMES",
+    "MANAGE_ROLES",
+    "MANAGE_WEBHOOKS",
+    "MANAGE_GUILD_EXPRESSIONS",
+    "USE_APPLICATION_COMMANDS",
+    "REQUEST_TO_SPEAK",
+    "MANAGE_EVENTS",
+    "MANAGE_THREADS",
+    "CREATE_PUBLIC_THREADS",
+    "CREATE_PRIVATE_THREADS",
+    "USE_EXTERNAL_STICKERS",
+    "SEND_MESSAGES_IN_THREADS",
+    "USE_EMBEDDED_ACTIVITIES",
+    "MODERATE_MEMBERS*",
+    "VIEW_CREATOR_MONETIZATION_ANALYTICS",
+    "USE_SOUNDBOARD",
+    "CREATE_GUILD_EXPRESSIONS",
+    "CREATE_EVENTS",
+    "USE_EXTERNAL_SOUNDS",
+    "SEND_VOICE_MESSAGES",
+    "SEND_POLLS"
+]
+class BitMapping {
+    constructor(keyMap, bits) {
+        this._bits = bits;
+        this._keys = Object.fromEntries(keyMap.map((name, idx) => [idx, name]));
+    }
+    get(key) {
+        return !!((this._bits >> this._keys[key]) & 1)
+    }
+    set(key, bool) {
+        this._bits = (this._bits & ~(bool << this._keys[key])) | (bool << this._keys[key])
+    }
+    merge(...bits) {
+        for (const pack of bits) {
+            this._bits |= pack?._bits ?? pack;
+        }
+    }
+    toBits() {
+        return this._bits;
+    }
+}
+const client = new ApiInterface();
+const main = document.getElementById('main');
+    main.setAttribute('style', `
+        ${main.getAttribute('style') /** preserve scaling style */}
+        overflow-y: hidden;
+    `);
+        const messageWrapper = document.createElement('div');
+                const messageList = document.createElement('ul');
+                    messageList.setAttribute('style', ` 
+                        display: flex;
+                        flex-direction: column-reverse;
+                        overflow-y: auto;
+                        height: 100%;
+                        list-style-type: none;
+                        padding: 0;
+                        margin: 0;
+                    `);
+            messageWrapper.appendChild(messageList)
+    main.appendChild(messageWrapper);
+        const messageInput = document.createElement('input');
+            messageInput.setAttribute('type', 'text')
+            main.appendChild(messageInput);
+messageInput.onkeydown = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+        client.fromApi(`POST /channels/${channel}/messages`, { content: messageInput.value });
+        messageInput.value = '';
+    }
+}
+
+function lastEl(elementArray) {
+    return elementArray[elementArray.length -1];
+}
+async function fetchMessage(msgId) {
+    if (client.messages[msgId]) return client.messages[msgId];
+    const message = (await client.fromApi(`GET /channels/${channel}/messages`, { limit: 1, around: msgId }))[0];
+    client.messages[msgId] = message;
+    if (Object.keys(client.messages).length > 200) 
+        delete client.messages[Object.keys(client.messages)[0]];
+    return message;
+}
+function makeMessageEl(message, compact) {
+    const li = document.createElement('li');
+        li.setAttribute('style', `
+            flex: 0 0 auto;
+        `);
+        li.setAttribute('id', message.id);
+            if (message.message_reference) {
+                // do not perform the fetch request on the message gen time
+                // if we do messages will be noticeably slower to appear
+                const replyWrapper = document.createElement('div');
+                    replyWrapper.setAttribute('style', `
+                        font-size: 0.7rem;
+                        padding-top: 8px;
+                        padding-left: 2.2rem;
+                        margin-bottom: -1.25rem;
+                    `)
+                        const em = document.createElement('em');
+                                em.innerText = 'Loading reply message';
+                            replyWrapper.appendChild(em);
+                    li.appendChild(replyWrapper); 
+                (async () => {
+                    const replied = await fetchMessage(message.message_reference.message_id);
+                    replyWrapper.innerHTML = '';
+                    replyWrapper.appendChild(Asset.UserAvatar(replied.author, 'png', 20, `
+                        display: inline;
+                        width: 0.8rem;
+                        height: 0.8rem;
+                        margin-bottom: -0.2rem;
+                        clip-path: circle();
+                    `));
+                    const name = document.createElement('div');
+                        name.setAttribute('style', `
+                            display: inline;
+                            font-weight: bold;
+                            padding-right: 4px;
+                            padding-left: 2px;
+                        `);
+                        name.setAttribute('class', 'name-text');
+                            name.innerText = replied.author.username;
+                        replyWrapper.appendChild(name);
+                    const content = document.createElement('div');
+                        content.setAttribute('style', `
+                            display: inline;
+                        `);
+                        content.setAttribute('class', 'content-wrapper');
+                            content.innerText = replied.content;
+                        replyWrapper.appendChild(content);
+                })();
+            }
+            if (compact) {
+                const content = document.createElement('div');
+                    content.setAttribute('style', `
+                        padding-left: 2.2rem;
+                    `)
+                    content.setAttribute('class', 'content-wrapper');
+                        content.innerText = message.content;
+                    li.appendChild(content);
+                return li;
+            }
+            li.appendChild(Asset.UserAvatar(message.author, 'png', 70, `
+                display: inline;
+                width: 2rem;
+                height: 2rem;
+                margin-bottom: -2.4rem;
+                clip-path: circle();
+            `));
+            const textWrapper = document.createElement('div');
+                textWrapper.setAttribute('style', `
+                    padding-left: 2.2rem;
+                `);
+                    const name = document.createElement('div');
+                        name.setAttribute('style', `
+                            font-weight: bold;
+                        `);
+                        name.setAttribute('class', 'name-text');
+                            name.innerText = message.author.username;
+                        textWrapper.appendChild(name);
+                    const content = document.createElement('div');
+                        content.setAttribute('style', ``)
+                        content.setAttribute('class', 'content-wrapper');
+                            content.innerText = message.content;
+                        textWrapper.appendChild(content);
+                li.appendChild(textWrapper);
+        return li;
+}
+function updateMessageEl(message) {
+    const messageEl = document.getElementById(message.id);
+    const pfp = messageEl.getElementsByTagName('img')[0];
+    const name = messageEl.getElementsByClassName('name-text');
+    const content = messageEl.getElementsByClassName('content-wrapper');
+    pfp.src = Asset.UserAvatar(message.author);
+    name.innerText = message.author.username;
+    content.innerHTML = marked.parse(message.content);
+}
+const oneHour = 3600000
+async function fillMessages() {
+    client.messages = {};
+    messageList.innerHTML = '';
+    const res = await client.fromApi(`GET /channels/${channel}/messages`, { limit: 100 });
+    if (res.message) throw res.message;
+    for (const [idx, message] of Object.entries(res)) {
+        const nextMessage = res[+(idx) +1];
+        if (nextMessage) nextMessage.timestamp = new Date(message.timestamp);
+        client.messages[message.id] = message;
+        if (typeof message.timestamp === 'string') 
+            message.timestamp = new Date(message.timestamp);
+        const shouldCompact = nextMessage 
+            && nextMessage.author.id === message.author.id
+            && (nextMessage.timestamp - message.timestamp) < oneHour
+            && !message.message_reference;
+        messageList.appendChild(makeMessageEl(message, shouldCompact));
+    }
+}
+Object.defineProperty(window, 'channel', {
+    get() {
+        return location.hash?.slice?.(1) ?? null;
+    },
+    set(id) {
+        location.hash = `#${id}`;
+        fillMessages();
+    } 
+});
+fillMessages();
+client.reqVisUpdate = async (event, message) => {
+    const first = messageList.children[0];
+    switch (event) {
+    case 'MESSAGE_CREATE': {
+        message.timestamp = new Date(message.timestamp);
+        const lastMessage = client.messages[first.getAttribute('id')];
+        const shouldCompact = lastMessage 
+            && lastMessage.author.id === message.author.id
+            && (lastMessage.timestamp - message.timestamp) < oneHour
+            && !message.message_reference;
+        messageList.insertBefore(makeMessageEl(message, shouldCompact), first);
+        lastEl(messageList.children).remove();
+        break;
+    }
+    case 'MESSAGE_UPDATE':
+        updateMessageEl(message);
+        break;
+    case 'MESSAGE_DELETE':
+        message.ids = [message.id];
+    case 'MESSAGE_DELETE_BULK':{
+        for (const id of message.ids) {
+            const messageEl = document.getElementById(id);
+            if (!messageEl) continue;
+            messageEl.remove();
+        }
+        const topMessage = messageList.children[0].getAttribute('id');
+        const newMessages = await client.fromApi(`GET /channels/${channel}/messages`, { 
+            limit: 100 - messageList.children.length, 
+            before: topMessage 
+        });
+        let lastMessage = null;
+        for (const message of newMessages) {
+            client.messages[message.id] = message;
+            message.timestamp = new Date(message.timestamp);
+            const shouldCompact = lastMessage 
+                && lastMessage.author.id === message.author.id
+                && (lastMessage.timestamp - message.timestamp) < oneHour
+                && !message.message_reference;
+            messageList.appendChild(makeMessageEl(message, shouldCompact));
+            lastMessage = message;
+        }
+        break;
+    }
+    }
+};
---
+++ discordnt/codeSrc/interface.js
@@ -0,0 +1,216 @@
+import './pako.js';
+// note: this is reversed to how it should actually be shaped
+const ZLIB_SUFFIX = new Uint8Array([255, 255, 0, 0]);
+const gateway = 'wss://gateway.discord.gg';
+
+export const dmServer = Symbol('@me')
+export default class ApiInterface {
+    constructor() {
+        this.mustAuthImediat = false;
+        this.reconUrl = gateway;
+        this.sessionId = null;
+        this.token = localStorage.token;
+        this.version = 10;
+        this.reqVisUpdate = () => {};
+        this.emojis = [];
+        this.stickers = [];
+        this.savedMedia = [];
+        this.messages = {};
+        this.channels = {};
+        this.guilds = { [dmServer]: [] };
+
+        this.msgBuf = new Uint8Array();
+        this.infContext = new pako.Inflate({
+            to: 'string',
+            chunkSize: 0xFFFFFF
+        });
+        this.infContext.onData = txt => {
+            let json;
+            try {
+                json = JSON.parse(txt);
+            } catch (err) {
+                console.log('invalid json', txt, err);
+            }
+            if (json) this.onpacket(json);
+        }
+        const url = new URL(gateway);
+        url.searchParams.set('v', this.version);
+        url.searchParams.set('encoding', 'json');
+        url.searchParams.set('compress', 'zlib-stream');
+        this.websocket = new WebSocket(url);
+        this.websocket.binaryType = "arraybuffer";
+        this.websocket.onopen = this.onopen.bind(this);
+        this.websocket.onmessage = this.onmessage.bind(this);
+        this.websocket.onerror = this.onerror.bind(this);
+        this.websocket.onclose = this.onclose.bind(this);
+    }
+
+    fromApi(callPath, body) {
+        const [method, path] = callPath.split(' ', 2);
+        const url = new URL('https://discord.com/api/v' + this.version + path);
+        console.log(method, 'at', url.toString());
+        const opts = {
+            method,
+            headers: {
+                'Authorization': this.token,
+                'Content-Type': 'application/json'
+            }
+        }
+        if (method === 'GET' && body) {
+            for (const [key, value] of Object.entries(body)) {
+                url.searchParams.set(key, value);
+            }
+        } else {
+            opts.body = JSON.stringify(body);
+        }
+
+        return fetch(url, opts).then(req => req.json()).catch(message => ({ message }));
+    }
+
+    reconnect(useGateway, message) {
+        console.warn('reconnecting because', message);
+        this.mustAuthImediat = !useGateway;
+        const reconUrl = useGateway
+            ? gateway
+            : this.reconUrl;
+        this.websocket.close();
+        const url = new URL(reconUrl);
+        url.searchParams.set('v', this.version);
+        url.searchParams.set('encoding', 'json');
+        url.searchParams.set('compress', 'zlib-stream');
+        this.websocket = new WebSocket(reconUrl);
+        this.websocket.binaryType = "arraybuffer";
+        this.websocket.onopen = this.onopen.bind(this);
+        this.websocket.onmessage = this.onmessage.bind(this);
+        this.websocket.onerror = this.onerror.bind(this);
+        this.websocket.onclose = this.onclose.bind(this);
+    }
+    onopen() {
+        if (this.mustAuthImediat) {
+            // always unset because this is for an explicit task that can not be reiterated elsewhen
+            this.mustAuthImediat = false;
+            this.send(6, {
+                token: this.token,
+                session_id: this.sessionId,
+                seq: this.seq
+            });
+        }
+    }
+    onmessage(e) {
+        const data = new Uint8Array(e.data);
+        const msgBuf = this.msgBuf;
+        this.msgBuf = new Uint8Array(msgBuf.length + data.length);
+        this.msgBuf.set(msgBuf);
+        this.msgBuf.set(data, msgBuf.length);
+        const isEnd = ZLIB_SUFFIX.every((v, i) => data[data.length - (i +1)] === v);
+        if (isEnd) {
+            this.infContext.push(this.msgBuf, 2);
+            this.msgBuf = new Uint8Array();
+        }
+    }
+    onpacket({ op: opcode, d: data, s: seq, t: event }) {
+        if (seq) this.seq = seq;
+        if (event) return this.onevent(event, data);
+        console.log('gateway op:', opcode, 'd:', data, 's:', seq, 't:', event);
+        switch (opcode) {
+        case 1:
+            this.send(11);
+            break;
+        case 7:
+            this.reconnect(false, 'server requested reconnect');
+            break;
+        case 9:
+            this.reconnect(true, 'invalid session');
+            break;
+        case 10:
+            this.heart = setInterval(() => {
+                if (this.waitingResponse) return this.reconnect(false, 'no pong to our ping')
+                this.send(1, this.seq)
+            }, data.heartbeat_interval);
+            this.send(2, {
+                "token": this.token,
+                "capabilities": 16381,
+                "properties": {
+                    "os": "Win32",
+                    "browser": "Mozilla",
+                    "device": "",
+                    "system_locale": navigator.language,
+                    "browser_user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+                    "browser_version": "5.0",
+                    "os_version": "21.0.0",
+                    "referrer": "",
+                    "referring_domain": "",
+                    "referrer_current": "",
+                    "referring_domain_current": "",
+                    "release_channel": "stable",
+                    "client_build_number": 291963,
+                    "client_event_source": null,
+                    "design_id": 0
+                },
+                "presence": {
+                    "status": "online",
+                    "since": 0,
+                    "activities": [],
+                    "afk": false
+                },
+                "compress": false,
+                "client_state": {
+                    "guild_versions": {}
+                }
+            });
+            break;
+        case 11:
+            this.waitingResponse = false;
+            break;
+        }
+    }
+    async onevent(event, data) {
+        switch (event) {
+        case 'READY':
+            for (const server of data.guilds) {
+                this.emojis = this.emojis.concat(server.emojis);
+                this.stickers = this.stickers.concat(server.stickers);
+                const channels = {};
+                for (const channel of server.channels) channels[channel.id] = channel;
+                for (const channel of server.threads) channels[channel.id] = channel;
+                Object.assign(this.channels, channels);
+                const roles = {};
+                for (const role of server.roles) roles[role.id] = role;
+                this.guilds[server.id] = {
+                    ...server.properties,
+                    channels,
+                    roles
+                };
+            }
+            console.log(this);
+            break;
+        case 'MESSAGE_EDIT': 
+        case 'MESSAGE_CREATE':
+            if (data.channel_id !== channel) break;
+            this.reqVisUpdate(event, data);
+            this.messages[data.id] = data;
+            if (Object.keys(this.messages).length > 200) 
+                delete this.messages[Object.keys(this.messages)[0]];
+            break;
+        case 'MESSAGE_DELETE':
+            if (data.channel_id !== channel) break;
+            this.reqVisUpdate(event, data);
+            delete this.messages[data.id];
+            break;
+        }
+    }
+    onerror() {
+        this.reconnect(false, 'websocket errored');
+    }
+    onclose() {
+        clearInterval(this.heart);
+    }
+
+    send(opcode, data) {
+        const obj = {
+            op: opcode,
+            d: data
+        };
+        this.websocket.send(JSON.stringify(obj));
+    }
+}
