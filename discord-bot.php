2bbde307fc5d81ee00283781b9a8fa6317b2bfdc
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2024-09-06 16:07:56 -0700

    minor format change

--- discord-bot.php
+++ discord-bot.php
@@ -1,24 +1,24 @@
 <?php
 // Interaction Type
-const PING = 1;
-const APPLICATION_COMMAND = 2;
-const MESSAGE_COMPONENT = 3;
+const PING                             = 1;
+const APPLICATION_COMMAND              = 2;
+const MESSAGE_COMPONENT                = 3;
 const APPLICATION_COMMAND_AUTOCOMPLETE = 4;
-const MODAL_SUBMIT = 5;
+const MODAL_SUBMIT                     = 5;
 // Interaction Context Types
-const GUILD = 0;
-const BOT_DM = 1;
+const GUILD           = 0;
+const BOT_DM          = 1;
 const PRIVATE_CHANNEL = 2;
 // Interaction Callback Type
-const PONG = 1;
-const CHANNEL_MESSAGE_WITH_SOURCE = 4;
-const DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5;
-const DEFERRED_UPDATE_MESSAGE = 6;
-const UPDATE_MESSAGE = 7;
+const PONG                                    = 1;
+const CHANNEL_MESSAGE_WITH_SOURCE             = 4;
+const DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE    = 5;
+const DEFERRED_UPDATE_MESSAGE                 = 6;
+const UPDATE_MESSAGE                          = 7;
 const APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8;
-const MODAL = 9;
-const PREMIUM_REQUIRED = 10;
-const LAUNCH_ACTIVITY = 12;
+const MODAL                                   = 9;
+const PREMIUM_REQUIRED                       = 10;
+const LAUNCH_ACTIVITY                        = 12;
 
 function DCHTTP($method, $endpoint, $body) {
     return file_get_contents("https://discord.com/api/v10$endpoint", false, stream_context_create([ 'http' => [
