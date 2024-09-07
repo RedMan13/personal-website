ad4b8f600bd68f7d51735da43cfe3d8b4ee10c45
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2024-09-06 20:42:50 -0700

    ok php wont do anything but kinda

--- discord-bot.php
+++ discord-bot.php
@@ -28,9 +28,8 @@ function DCHTTP($method, $endpoint, $body) {
     ] ]));
 }
 
-header('Content-type: application/json');
+header('Content-type: application/json; charset=us-ascii');
 $data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('./debug.txt', $data['type'] == PING);
 $appId = $data['application_id'];
 $token = $data['token'];
 if ($data['type'] == PING) {
