031c27afb1c3c684f8e7e648f650b0558ffa2d72
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2024-09-06 17:48:44 -0700

    guh

--- discord-bot.php
+++ discord-bot.php
@@ -30,6 +30,7 @@ function DCHTTP($method, $endpoint, $body) {
 
 header('Content-type: application/json');
 $data = json_decode(file_get_contents('php://input'), true);
+file_put_contents('../debug.json', json_encode($data));
 if ($data['type'] == PING) {
     echo json_encode([ 'type' => PONG ]);
     exit;
