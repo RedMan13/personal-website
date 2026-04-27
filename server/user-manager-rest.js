8603b48e416782113502d6ebce1542b016f2cfa6
RedMan13 <godslayerakp@gmail.com>
2026-04-27 14:12:37 -0700

    skibidi bop bop yes yes

--- server/user-manager-rest.js
+++ server/user-manager-rest.js
@@ -1,3 +1,11 @@
+const { handleReject, codes } = require('./handle-reject.js');
+
 module.exports = server => {
-    
+    server.get('/logs', (req, res) => {
+        if (!req.authorized || !await users.userExists(req.cookies.username))
+            return handleReject(codes.Forbidden, 'You must login to be able to view this content', res, false);
+        if (!await users.canUse(req.cookies.username, 'view-logs'))
+            return handleReject(codes.Forbidden, 'You are not permitted to view this content', res, false);
+        res.file('../logs/errors.log');
+    })
 }
